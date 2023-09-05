238e05fd7ef68d88ebaa52a2a16098837bede855
lashagelashvili <las.gelashvili@gmail.com>
2023-09-05 23:32:35 +0400

    contributions page

---
+++ src/app/[user]/[year]/page.tsx
@@ -0,0 +1,39 @@
+import { Params } from "./params.model";
+
+export default async function Contributions({
+  params: { user, year },
+}: Params) {
+  const res = await fetch(`http://localhost:3000/api/${user}/${year}`);
+
+  let data = await res.json();
+
+  let total = 0;
+
+  data.forEach((row: any) => {
+    row.forEach((day: any) => {
+      if (day) {
+        total += day.count;
+      }
+    });
+  });
+
+  return (
+    <>
+      <h2>
+        {total} contributions in {year}
+      </h2>
+
+      <table>
+        {data.map((row: any, rowIndex: number) => (
+          <tbody key={rowIndex}>
+            <tr key={rowIndex}>
+              {row.map((day: any, dayIndex: number) => (
+                <td key={dayIndex} data-level={day?.level}></td>
+              ))}
+            </tr>
+          </tbody>
+        ))}
+      </table>
+    </>
+  );
+}
