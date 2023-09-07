31dba22ec3935005af808efd0e7241adc55b4918
lashagelashvili <las.gelashvili@gmail.com>
2023-09-07 17:52:01 +0400

    redirect user on load

--- src/app/[user]/[year]/page.tsx
+++ src/app/[user]/[year]/page.tsx
@@ -1,7 +1,7 @@
 import { Params } from "./params.model";
 
 export default async function Contributions({
-  params: { user, year },
+  params: { user = "lashagelashvili", year = "2023" },
 }: Params) {
   const res = await fetch(`http://localhost:3000/api/${user}/${year}`);
 
